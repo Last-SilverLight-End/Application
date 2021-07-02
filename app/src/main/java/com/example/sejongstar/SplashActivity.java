a262c521b577929551c7c30faccf4f59066a82e3
Last-SilverLight-End <halfwing1998@gmail.com>
2021-07-02 22:20:15 +0900

    test

---
+++ app/src/main/java/com/example/sejongstar/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.sejongstar;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
